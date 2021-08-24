ce72c017cc2746f6af8253c1668ee53db8971b75
neneprm <s.prima@hotmail.com>
2021-08-24 22:09:13 +0700

    Created class Comment

---
+++ app/src/main/java/com/gin_arai_dee/Comment.java
@@ -0,0 +1,37 @@
+package com.gin_arai_dee;
+
+public class Comment {
+//    private User user;
+    private final String text;
+    private final Integer wordCount;
+    private Integer voteCount;
+    private final Integer MAX_WORD_COUNT;
+
+    public Comment() {
+        this.text = "";
+        this.wordCount = 0;
+        this.voteCount = 0;
+        this.MAX_WORD_COUNT = 100;
+    }
+
+    public Comment(String text, Integer wordCount, Integer voteCount) {
+        this.text = text;
+        this.wordCount = wordCount;
+        this.voteCount = voteCount;
+        this.MAX_WORD_COUNT = 100;
+    }
+
+    public boolean isValid() { return wordCount <= MAX_WORD_COUNT; }
+
+//    public void uploadComment() {
+//
+//    }
+
+    public void upvote() { voteCount++; }
+
+    public void downvote() { voteCount--; }
+
+//    public void deleteComment {
+//
+//    }
+}
