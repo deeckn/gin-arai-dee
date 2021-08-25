0aabf8f09be702e513be200da457d31abf9b7b73
Dee Deesit <dee28511@gmail.com>
2021-08-25 23:31:13 +0700

    Added UserHistory class

---
+++ app/src/main/java/com/gin_arai_dee/UserHistory.java
@@ -0,0 +1,17 @@
+package com.gin_arai_dee;
+
+import java.util.ArrayList;
+
+public class UserHistory {
+    private final String tableName;
+//    private ArrayList<Recipe> recipeBlogs;
+
+    UserHistory(String tableName) {
+        this.tableName = tableName;
+    }
+
+    private void loadData() {}
+    private void saveData() {}
+    private void deleteRecipe() {}
+    private void createUserHistory() {}
+}
