7cb53ddcd416b2711373ce5101ab166526f3961e
Dee Deesit <dee28511@gmail.com>
2021-08-31 09:57:47 +0700

    Removed action bar banner

--- app/src/main/java/com/gin_arai_dee/MainActivity.java
+++ app/src/main/java/com/gin_arai_dee/MainActivity.java
@@ -3,12 +3,17 @@ package com.gin_arai_dee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
     }
 }
