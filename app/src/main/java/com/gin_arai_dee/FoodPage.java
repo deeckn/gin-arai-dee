07bfcc286461184eab7d07b526d4bee6f7c976a5
Dee Deesit <dee28511@gmail.com>
2021-09-09 23:00:50 +0700

    Food page latest version, still broken

--- app/src/main/java/com/gin_arai_dee/FoodPage.java
+++ app/src/main/java/com/gin_arai_dee/FoodPage.java
@@ -1,41 +1,77 @@
 package com.gin_arai_dee;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.animation.LayoutTransition;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+
 
 public class FoodPage extends AppCompatActivity {
+
+    // Navigation Bar
+    BottomNavigationView bottomNavigationView;
+
+    // Category Selector Box
+    GridLayout categorySelectorBox;
+    ImageButton dropdownTriangleArrow;
+    boolean categoryBoxState = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_page);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.dock_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.food_page);
+        // Initializing page elements
+        bottomNavigationView = findViewById(R.id.dock_navigation);
+        categorySelectorBox = findViewById(R.id.category_selector_box);
+        dropdownTriangleArrow = findViewById(R.id.category_arrow);
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home_page:
-                        startActivity(new Intent(getApplicationContext(), HomePage.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.food_page:
-                        break;
-                    default:
-                        System.out.println("Not Implemented");
-                }
-                return false;
+        // Navigation Bar Settings
+        bottomNavigationView.setSelectedItemId(R.id.food_page);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home_page:
+                    startActivity(new Intent(getApplicationContext(), HomePage.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                case R.id.food_page:
+                    break;
+                default:
+                    System.out.println("Not Implemented");
             }
+            return false;
         });
+    }
+
+    // Expand and minimize category selector box
+    public void toggleCategoryBox(View view) {
+        GridLayout.LayoutParams boxDimensions = new GridLayout.LayoutParams();
+        boxDimensions.width = categorySelectorBox.getWidth();
+        boxDimensions.setGravity(Gravity.CENTER);
+        int boxMargins = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 20, getResources()
+                        .getDisplayMetrics());
+        boxDimensions.setMargins(boxMargins, boxMargins, boxMargins, 0);
+
+        if (categoryBoxState) {
+            dropdownTriangleArrow.animate().rotation(90).setDuration(500);
+            boxDimensions.height = 400;
+        }
+        else {
+            dropdownTriangleArrow.animate().rotation(0).setDuration(500);
+            boxDimensions.height = GridLayout.LayoutParams.WRAP_CONTENT;
+        }
+
+        LayoutTransition transition = categorySelectorBox.getLayoutTransition();
+        transition.enableTransitionType(LayoutTransition.CHANGING);
+        categorySelectorBox.setLayoutParams(boxDimensions);
+        categoryBoxState = !categoryBoxState;
     }
 }
