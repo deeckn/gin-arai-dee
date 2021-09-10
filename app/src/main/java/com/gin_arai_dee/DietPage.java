fcba2ce891b92f277f1ad36ea2363e3cba2b696b
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-09-10 21:32:37 +0700

    Diet Layout

---
+++ app/src/main/java/com/gin_arai_dee/DietPage.java
@@ -0,0 +1,15 @@
+package com.gin_arai_dee;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DietPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_page);
+    }
+}
