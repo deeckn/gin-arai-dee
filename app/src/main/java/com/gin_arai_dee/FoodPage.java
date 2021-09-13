22c7dee647f52e7da9d94407443c717538ea1737
Dee Deesit <dee28511@gmail.com>
2021-09-13 15:30:45 +0700

    Added nationality selectors and optimizing object creation

--- app/src/main/java/com/gin_arai_dee/FoodPage.java
+++ app/src/main/java/com/gin_arai_dee/FoodPage.java
@@ -6,6 +6,7 @@ import androidx.core.content.res.ResourcesCompat;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.GridLayout;
@@ -24,8 +25,9 @@ public class FoodPage extends AppCompatActivity {
 
     // Category Selector Box
     GridLayout categorySelectorBox;
-    ImageButton dropdownTriangleArrow;
+    ImageButton categoryDropdownArrow;
     boolean categoryBoxState = true;
+    boolean categoryLoaded = false;
     ArrayList<TextView> categoryTextViews;
     TextView main_dish_text;
     TextView appetizer_text;
@@ -39,6 +41,24 @@ public class FoodPage extends AppCompatActivity {
     CheckBox snacks_checkbox;
     CheckBox beverages_checkbox;
 
+    // Nationality Selector Box
+    GridLayout nationalitySelectorBox;
+    ImageButton nationalityDropdownArrow;
+    boolean nationalityBoxState = true;
+    boolean nationalityLoaded = false;
+    ArrayList<TextView> nationalityTextViews;
+    TextView thai_text;
+    TextView japanese_text;
+    TextView italian_text;
+    TextView chinese_text;
+    TextView korean_text;
+    ArrayList<CheckBox> nationalityCheckBoxes;
+    CheckBox thai_checkbox;
+    CheckBox japanese_checkbox;
+    CheckBox italian_checkbox;
+    CheckBox chinese_checkbox;
+    CheckBox korean_checkbox;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,10 +67,16 @@ public class FoodPage extends AppCompatActivity {
         // Initializing page elements and objects
         parentGridLayout = findViewById(R.id.main_grid_layout);
         bottomNavigationView = findViewById(R.id.dock_navigation);
+
         categorySelectorBox = findViewById(R.id.category_selector_box);
-        dropdownTriangleArrow = findViewById(R.id.category_arrow);
+        categoryDropdownArrow = findViewById(R.id.category_arrow);
         categoryTextViews = new ArrayList<>();
         categoryCheckBoxes = new ArrayList<>();
+
+        nationalitySelectorBox = findViewById(R.id.nationality_selector_box);
+        nationalityDropdownArrow = findViewById(R.id.nationality_arrow);
+        nationalityTextViews = new ArrayList<>();
+        nationalityCheckBoxes = new ArrayList<>();
 
         // Navigation Bar Settings
         bottomNavigationView.setSelectedItemId(R.id.food_page);
@@ -71,75 +97,81 @@ public class FoodPage extends AppCompatActivity {
         });
     }
 
-    public int getValueInDp(int value) {
+    private int getValueInDp(int value) {
         float scale = getResources().getDisplayMetrics().density;
         return (int) (value * scale + 0.5f);
     }
 
     // Expand and minimize category selector box
     public void toggleCategoryBox(View view) {
-        // New Elements Creation
-        main_dish_text = new TextView(this);
-        appetizer_text = new TextView(this);
-        desserts_text = new TextView(this);
-        snacks_text = new TextView(this);
-        beverages_text = new TextView(this);
-        main_dish_checkbox = new CheckBox(this);
-        appetizer_checkbox = new CheckBox(this);
-        desserts_checkbox = new CheckBox(this);
-        snacks_checkbox = new CheckBox(this);
-        beverages_checkbox = new CheckBox(this);
+        if (!categoryLoaded) {
+            // New Elements Creation
+            main_dish_text = new TextView(this);
+            appetizer_text = new TextView(this);
+            desserts_text = new TextView(this);
+            snacks_text = new TextView(this);
+            beverages_text = new TextView(this);
 
-        // TextViews for Category Titles
-        categoryTextViews.clear();
-        categoryTextViews.add(main_dish_text);
-        categoryTextViews.add(appetizer_text);
-        categoryTextViews.add(desserts_text);
-        categoryTextViews.add(snacks_text);
-        categoryTextViews.add(beverages_text);
+            main_dish_checkbox = new CheckBox(this);
+            appetizer_checkbox = new CheckBox(this);
+            desserts_checkbox = new CheckBox(this);
+            snacks_checkbox = new CheckBox(this);
+            beverages_checkbox = new CheckBox(this);
 
-        // Checkboxes for each category titles
-        categoryCheckBoxes.clear();
-        categoryCheckBoxes.add(main_dish_checkbox);
-        categoryCheckBoxes.add(appetizer_checkbox);
-        categoryCheckBoxes.add(desserts_checkbox);
-        categoryCheckBoxes.add(snacks_checkbox);
-        categoryCheckBoxes.add(beverages_checkbox);
+            // TextViews for Category Titles
+            categoryTextViews.clear();
+            categoryTextViews.add(main_dish_text);
+            categoryTextViews.add(appetizer_text);
+            categoryTextViews.add(desserts_text);
+            categoryTextViews.add(snacks_text);
+            categoryTextViews.add(beverages_text);
 
-        // Category TextView Setup
-        main_dish_text.setText(R.string.main_dish);
-        appetizer_text.setText(R.string.beverages);
-        desserts_text.setText(R.string.desserts);
-        snacks_text.setText(R.string.snacks);
-        beverages_text.setText(R.string.beverages);
-        
-        Typeface rubik_regular = ResourcesCompat.getFont(this, R.font.rubik);
+            // Checkboxes for each category titles
+            categoryCheckBoxes.clear();
+            categoryCheckBoxes.add(main_dish_checkbox);
+            categoryCheckBoxes.add(appetizer_checkbox);
+            categoryCheckBoxes.add(desserts_checkbox);
+            categoryCheckBoxes.add(snacks_checkbox);
+            categoryCheckBoxes.add(beverages_checkbox);
 
-        for (TextView t : categoryTextViews) {
-            t.setTextSize(18);
-            t.setPadding(getValueInDp(20), 0, 0, 0);
-            t.setTypeface(rubik_regular);
+            // Category TextView Setup
+            main_dish_text.setText(R.string.main_dish);
+            appetizer_text.setText(R.string.beverages);
+            desserts_text.setText(R.string.desserts);
+            snacks_text.setText(R.string.snacks);
+            beverages_text.setText(R.string.beverages);
+
+            Typeface rubik_regular = ResourcesCompat.getFont(this, R.font.rubik);
+
+            for (TextView t : categoryTextViews) {
+                t.setTextSize(18);
+                t.setPadding(getValueInDp(20), 0, 0, 0);
+                t.setTypeface(rubik_regular);
+            }
+
+            for (CheckBox c : categoryCheckBoxes) {
+                c.setGravity(Gravity.END);
+                c.setPadding(getValueInDp(20), 0, 0, 0);
+            }
+
+            categoryTextViews.get(categoryTextViews.size()-1)
+                    .setPadding(getValueInDp(20), 0, 0, getValueInDp(8));
+            categoryCheckBoxes.get(categoryCheckBoxes.size()-1)
+                    .setPadding(0, 0, getValueInDp(5), getValueInDp(8));
+
+            categoryLoaded = true;
         }
-
-        for (CheckBox c : categoryCheckBoxes) {
-            c.setPadding(getValueInDp(20), 0, 0, 0);
-        }
-
-        categoryTextViews.get(categoryTextViews.size()-1)
-                .setPadding(getValueInDp(20), 0, 0, getValueInDp(8));
-        categoryCheckBoxes.get(categoryCheckBoxes.size()-1)
-                .setPadding(0, 0, getValueInDp(5), getValueInDp(8));
 
         // Performing tasks based on CategoryBoxState boolean
         if (categoryBoxState) {
-            dropdownTriangleArrow.animate().rotation(90).setDuration(500);
+            categoryDropdownArrow.animate().rotation(90).setDuration(500);
             for (int i = 0; i < categoryTextViews.size(); i++) {
                 categorySelectorBox.addView(categoryTextViews.get(i));
                 categorySelectorBox.addView(categoryCheckBoxes.get(i));
             }
         }
         else {
-            dropdownTriangleArrow.animate().rotation(0).setDuration(500);
+            categoryDropdownArrow.animate().rotation(0).setDuration(500);
             for (int i = 0; i < categoryTextViews.size() * 2; i++)
                 categorySelectorBox.removeViewAt(2);
         }
@@ -147,6 +179,78 @@ public class FoodPage extends AppCompatActivity {
     }
 
     public void toggleNationalityBox(View view) {
+        if (!nationalityLoaded) {
+            // New TextView and Checkbox Creation
+            thai_text = new TextView(this);
+            japanese_text = new TextView(this);
+            italian_text = new TextView(this);
+            chinese_text = new TextView(this);
+            korean_text = new TextView(this);
 
+            thai_checkbox = new CheckBox(this);
+            japanese_checkbox = new CheckBox(this);
+            italian_checkbox = new CheckBox(this);
+            chinese_checkbox = new CheckBox(this);
+            korean_checkbox = new CheckBox(this);
+
+            // TextViews for Nationality Titles
+            nationalityTextViews.add(thai_text);
+            nationalityTextViews.add(japanese_text);
+            nationalityTextViews.add(italian_text);
+            nationalityTextViews.add(chinese_text);
+            nationalityTextViews.add(korean_text);
+
+            // Checkboxes for each nationality titles
+            nationalityCheckBoxes.add(thai_checkbox);
+            nationalityCheckBoxes.add(japanese_checkbox);
+            nationalityCheckBoxes.add(italian_checkbox);
+            nationalityCheckBoxes.add(chinese_checkbox);
+            nationalityCheckBoxes.add(korean_checkbox);
+
+            // Category TextView Setup
+            thai_text.setText(R.string.thai);
+            japanese_text.setText(R.string.japanese);
+            italian_text.setText(R.string.italian);
+            chinese_text.setText(R.string.chinese);
+            korean_text.setText(R.string.korean);
+
+            Typeface rubik_regular = ResourcesCompat.getFont(this, R.font.rubik);
+
+            for (TextView t : nationalityTextViews) {
+                t.setTextSize(18);
+                t.setPadding(getValueInDp(20), 0, 0, 0);
+                t.setTypeface(rubik_regular);
+            }
+
+            for (CheckBox c : nationalityCheckBoxes) {
+                c.setGravity(Gravity.END);
+                c.setPadding(getValueInDp(20), 0, 0, 0);
+            }
+
+            nationalityTextViews.get(nationalityTextViews.size()-1)
+                    .setPadding(getValueInDp(20), 0, 0, getValueInDp(8));
+            nationalityCheckBoxes.get(nationalityCheckBoxes.size()-1)
+                    .setPadding(0, 0, getValueInDp(5), getValueInDp(8));
+
+            nationalityLoaded = true;
+        }
+
+        // Performing tasks based on CategoryBoxState boolean
+        if (nationalityBoxState) {
+            nationalityDropdownArrow.animate().rotation(90).setDuration(500);
+            for (int i = 0; i < nationalityTextViews.size(); i++) {
+                nationalitySelectorBox.addView(nationalityTextViews.get(i));
+                nationalitySelectorBox.addView(nationalityCheckBoxes.get(i));
+            }
+        }
+        else {
+            nationalityDropdownArrow.animate().rotation(0).setDuration(500);
+            for (int i = 0; i < nationalityTextViews.size() * 2; i++)
+                nationalitySelectorBox.removeViewAt(2);
+        }
+        nationalityBoxState = !nationalityBoxState;
     }
+
+    private void clearFoodList() {}
+    private void buildFoodList() {}
 }
