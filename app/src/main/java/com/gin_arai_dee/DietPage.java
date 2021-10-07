4f9eed8b4ed963d9ff69a9274e5ed4a668c88169
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-10-07 19:46:02 +0700

    Added Dietary element

--- app/src/main/java/com/gin_arai_dee/DietPage.java
+++ app/src/main/java/com/gin_arai_dee/DietPage.java
@@ -1,15 +1,54 @@
 package com.gin_arai_dee;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DietPage extends AppCompatActivity {
 
+    // 7Days textview
+    TextView mondayCalorie;
+    TextView tuesdayCalorie;
+    TextView wednesdayCalorie;
+    TextView thursdayCalorie;
+    TextView fridayCalorie;
+    TextView saturdayCalorie;
+    TextView sundayCalorie;
+
+    // 7Days button
+    Button mondayButton;
+    Button tuesdayButton;
+    Button wednesdayButton;
+    Button thursdayButton;
+    Button fridayButton;
+    Button saturdayButton;
+    Button sundayButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_page);
+
+        // Initializing calories textview
+        mondayCalorie = findViewById(R.id.monday_kcal);
+        tuesdayCalorie = findViewById(R.id.tuesday_kcal);
+        wednesdayCalorie = findViewById(R.id.wednesday_kcal);
+        thursdayCalorie = findViewById(R.id.thursday_kcal);
+        fridayCalorie = findViewById(R.id.friday_kcal);
+        saturdayCalorie = findViewById(R.id.saturday_kcal);
+        sundayCalorie = findViewById(R.id.sunday_kcal);
+
+        // Initializing button
+        mondayButton = findViewById(R.id.monday_edit_button);
+        tuesdayButton = findViewById(R.id.tuesday_edit_button);
+        wednesdayButton = findViewById(R.id.wednesday_edit_button);
+        thursdayButton = findViewById(R.id.thursday_edit_button);
+        fridayButton = findViewById(R.id.friday_edit_button);
+        saturdayButton = findViewById(R.id.saturday_edit_button);
+        sundayButton = findViewById(R.id.sunday_edit_button);
+
     }
 }
