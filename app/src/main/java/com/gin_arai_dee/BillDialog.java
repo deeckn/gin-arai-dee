664c7b3b51e3d06b140a0b691c96001c6f103432
neneprm <s.prima@hotmail.com>
2021-10-12 17:35:46 +0700

    Updated food dialog ui

---
+++ app/src/main/java/com/gin_arai_dee/BillDialog.java
@@ -0,0 +1,32 @@
+package com.gin_arai_dee;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class BillDialog extends DialogFragment {
+    public void show_dialog() { }
+
+    public static BillDialog newInstance(String title) {
+        BillDialog frag = new BillDialog();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_bill_food_dialog, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
