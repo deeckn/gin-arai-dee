6ec78e8e1364630ec14be0a40deac1f9825f63a6
neneprm <s.prima@hotmail.com>
2021-10-14 01:22:31 +0700

    Add person to bill

--- app/src/main/java/com/gin_arai_dee/BillSplitterPage.java
+++ app/src/main/java/com/gin_arai_dee/BillSplitterPage.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ScrollView;
 import android.widget.TabHost;
 import android.widget.TabWidget;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class BillSplitterPage extends AppCompatActivity {
     private class FoodItem {
@@ -35,8 +40,24 @@ public class BillSplitterPage extends AppCompatActivity {
     private class Person {
         private String name;
         private int payment;
+        private TextView name_textView;
+        private TextView payment_textView;
 
-        public Person(String name) { this.name = name; }
+        public Person(String name) {
+            this.name = name;
+            this.payment = 0;
+        }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+
+        public int getPayment() { return payment; }
+        public void setPayment(int payment) { this.payment = payment; }
+
+        public void setTextView(int nameId, int paymentId) {
+            name_textView = findViewById(nameId);
+            payment_textView = findViewById(paymentId);
+        }
     }
 
     private void showFoodDialog() {
@@ -45,10 +66,21 @@ public class BillSplitterPage extends AppCompatActivity {
         billDialog.show(fm, "fragment_edit_name");
     }
 
+    // Initialize UI
     ScrollView scrollView;
     EditText list_input;
     TabHost tabHost;
     Button add_list;
+    Button add_name;
+    EditText name_add_bar;
+    GridLayout name_list;
+    TextView person_name;
+    TextView person_payment;
+
+    // Initialize Person
+    Person person;
+    ArrayList<Person> people = new ArrayList<Person>();
+    String name_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +119,40 @@ public class BillSplitterPage extends AppCompatActivity {
             public void onClick(View v) {
 
                 showFoodDialog();
+            }
+        });
+
+        add_name = findViewById(R.id.add_name_button);
+        name_add_bar = findViewById(R.id.name_add_bar);
+        name_list = findViewById(R.id.name_list);
+
+        name_list.setColumnCount(2);
+        add_name.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                name_input = name_add_bar.getText().toString();
+                if (TextUtils.isEmpty(name_input)) System.out.println("empty");
+                else {
+                    System.out.println(name_input);
+                    person = new Person(name_input);
+                    people.add(person);
+
+                    person_name = new TextView(getApplicationContext());
+                    person_name.setText(name_add_bar.getText());
+                    person_name.setTypeface(getResources().getFont(R.font.rubik));
+                    person_name.setTextSize(20);
+                    person_name.setTextColor(getResources().getColor(R.color.ghost_white));
+
+                    person_payment = new TextView(getApplicationContext());
+                    person_payment.setText(String.valueOf(person.getPayment()));
+                    person_payment.setTypeface(getResources().getFont(R.font.rubik));
+                    person_payment.setTextSize(20);
+                    person_payment.setTextColor(getResources().getColor(R.color.ghost_white));
+
+                    name_add_bar.setText("");
+                    name_list.addView(person_name);
+                    name_list.addView(person_payment);
+                    person.setTextView(person_name.getId(), person_payment.getId());
+                }
             }
         });
     }
