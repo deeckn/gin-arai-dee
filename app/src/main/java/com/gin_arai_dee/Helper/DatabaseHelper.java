3492ee641cc942e50349fffc8e8cb809ebf6fb27
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-10-28 00:54:55 +0700

    Created Helper package

--- app/src/main/java/com/gin_arai_dee/Helper/DatabaseHelper.java
+++ app/src/main/java/com/gin_arai_dee/Helper/DatabaseHelper.java
@@ -1,4 +1,4 @@
-package com.gin_arai_dee;
+package com.gin_arai_dee.Helper;
 
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
@@ -6,8 +6,12 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.gin_arai_dee.Domain.FoodItem;
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
