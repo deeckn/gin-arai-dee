b859c65f2e406d9107edb5148a68562c1f76bdd4
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-10-30 16:38:49 +0700

    Created DietCardHolder

---
+++ app/src/main/java/com/gin_arai_dee/Holder/DietCardHolder.java
@@ -0,0 +1,19 @@
+package com.gin_arai_dee.Holder;
+
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import com.gin_arai_dee.R;
+
+import androidx.annotation.NonNull;
+
+public class DietCardHolder extends CardHolder{
+    public TextView cardTimeText;
+    public GridLayout cardGridLayout;
+
+    public DietCardHolder(@NonNull View itemView) {
+        super(itemView);
+        this.cardTimeText = itemView.findViewById(R.id.card_time);
+        this.cardGridLayout = itemView.findViewById(R.id.foodList_grid);
+    }
+}
