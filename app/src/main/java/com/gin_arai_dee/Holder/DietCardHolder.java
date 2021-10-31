1dfa1c9d0cc260bd09bc18225db9aba072408bfc
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-10-31 21:03:49 +0700

    Reformat Code

--- app/src/main/java/com/gin_arai_dee/Holder/DietCardHolder.java
+++ app/src/main/java/com/gin_arai_dee/Holder/DietCardHolder.java
@@ -3,9 +3,13 @@ package com.gin_arai_dee.Holder;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
+
+import com.gin_arai_dee.Domain.FoodItem;
 import com.gin_arai_dee.R;
 
 import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
 
 public class DietCardHolder extends CardHolder{
     public TextView cardTimeText;
