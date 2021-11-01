5e59802a252ad6bf7b6a0b6b8ac43b0b3402954a
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-11-02 00:53:57 +0700

    Fixed CardDietAdapter

--- app/src/main/java/com/gin_arai_dee/CardDietAdapter.java
+++ app/src/main/java/com/gin_arai_dee/CardDietAdapter.java
@@ -23,7 +23,7 @@ public class CardDietAdapter extends RecyclerView.Adapter<CardDietHolder> {
     @NonNull
     @Override
     public CardDietHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_card, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_diet, null);
         return new CardDietHolder(view);
     }
 
