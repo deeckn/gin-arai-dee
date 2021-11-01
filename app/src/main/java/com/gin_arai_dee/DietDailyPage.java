43254db0c74a0950fca30a197bcaf38a8d2b5e90
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-11-01 22:03:07 +0700

    Edited DietDailyPage

---
+++ app/src/main/java/com/gin_arai_dee/DietDailyPage.java
@@ -0,0 +1,35 @@
+package com.gin_arai_dee;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DietDailyPage extends AppCompatActivity {
+
+    TextView selectedDay;
+    TextView totalKcal;
+    RecyclerView recyclerView;
+    CardDietAdapter cardAdapter;
+    ArrayList<CardDietModel> foodItemLists;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_page_daily);
+
+        foodItemLists = new ArrayList<>();
+
+        selectedDay = findViewById(R.id.selectedDay);
+        totalKcal = findViewById(R.id.total_kcal);
+        recyclerView = findViewById(R.id.item_list);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cardAdapter = new CardDietAdapter(this,foodItemLists);
+    }
+}
