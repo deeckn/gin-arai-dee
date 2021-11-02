73f9856d8fe22f4c131675f48fddfecbba2df4b4
deeckn <dee28511@gmail.com>
2021-11-02 17:32:29 +0700

    Removed redundant setters

--- app/src/main/java/com/gin_arai_dee/FoodCardModel.java
+++ app/src/main/java/com/gin_arai_dee/FoodCardModel.java
@@ -1,10 +1,10 @@
 package com.gin_arai_dee;
 
 public class FoodCardModel {
-    private String title;
-    private String description;
-    private String calories;
-    private String image_url;
+    private final String title;
+    private final String description;
+    private final String calories;
+    private final String image_url;
 
     public FoodCardModel(String title, String description, String calories, String image_url) {
         this.title = title;
@@ -17,31 +17,15 @@ public class FoodCardModel {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getCalories() {
         return calories;
     }
 
-    public void setCalories(String calories) {
-        this.calories = calories;
-    }
-
     public String getImage_url() {
         return image_url;
-    }
-
-    public void setImage_url(String image_url) {
-        this.image_url = image_url;
     }
 }
