565fc743afdc3d328402a8a29866289aaf1c43ff
beammi <beamratchwalee@gmail.com>
2021-11-05 17:49:04 +0700

    Updated

--- app/src/main/java/com/gin_arai_dee/HomePage.java
+++ app/src/main/java/com/gin_arai_dee/HomePage.java
@@ -27,7 +27,7 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        initializeDatabase();
+        //initializeDatabase();
 
         // Navigation Settings
         bottomNavigationView = findViewById(R.id.dock_navigation);
--- app/src/main/java/com/gin_arai_dee/MainActivity.java
+++ app/src/main/java/com/gin_arai_dee/MainActivity.java
@@ -3,6 +3,7 @@ package com.gin_arai_dee;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(v -> openHomePage());
+
     }
 
     // Opens the home page
