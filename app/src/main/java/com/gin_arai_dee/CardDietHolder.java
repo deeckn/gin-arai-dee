4725a6e4704117489f328ce629af3c2a344931f3
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-11-06 01:39:03 +0700

    Added listView

--- app/src/main/java/com/gin_arai_dee/CardDietHolder.java
+++ app/src/main/java/com/gin_arai_dee/CardDietHolder.java
@@ -2,6 +2,7 @@ package com.gin_arai_dee;
 
 
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,9 +11,11 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CardDietHolder extends RecyclerView.ViewHolder {
 
     TextView dietTime;
+    ListView listView;
 
     public CardDietHolder(@NonNull View itemView) {
         super(itemView);
         dietTime = itemView.findViewById(R.id.card_time);
+        listView = itemView.findViewById(R.id.card_itemList);
     }
 }
