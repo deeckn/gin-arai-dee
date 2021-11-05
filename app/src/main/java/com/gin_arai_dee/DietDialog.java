13971ade75277159075d25a6bde7c771889e1bf3
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-11-06 05:28:55 +0700

    Added onClickEvent

--- app/src/main/java/com/gin_arai_dee/DietDialog.java
+++ app/src/main/java/com/gin_arai_dee/DietDialog.java
@@ -1,13 +1,14 @@
 package com.gin_arai_dee;
 
-import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -15,13 +16,11 @@ import android.widget.TimePicker;
 
 import androidx.fragment.app.DialogFragment;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
-import java.util.Objects;
 
 public class DietDialog extends DialogFragment {
     private static final String TAG = "DietDialog";
@@ -34,10 +33,13 @@ public class DietDialog extends DialogFragment {
     private EditText searchInput;
     private ListView itemList;
 
-
-    private int hour,minute;
+    DialogFoodAdapter adapter;
+    private int hour, minute;
     private String time_selected;
     private ArrayList<FoodItem> foodItems;
+    private ArrayList<FoodItem> selectedItems;
+    private ArrayList<FoodItem> displayItem;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_diet_page, container, false);
@@ -49,39 +51,82 @@ public class DietDialog extends DialogFragment {
         itemList = view.findViewById(R.id.dialog_item_list);
 
         foodItems = (ArrayList<FoodItem>) db.getAllFoodItems();
+        displayItem = foodItems;
 
-        DialogFoodAdapter  adapter = new DialogFoodAdapter(getContext(),R.layout.dialog_foodview,foodItems);
+        adapter = new DialogFoodAdapter(getContext(), R.layout.dialog_foodview, foodItems);
         itemList.setAdapter(adapter);
 
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                displayItem.get(position).setSelected(!displayItem.get(position).isSelected());
+                filter(searchInput.getEditableText().toString());
+            }
+        });
 
-        saveButton.setOnClickListener(e ->{
-            Log.d(TAG,"onClick: saveButton dialog");
+        // search input
+        searchInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                filter(editable.toString());
+            }
+        });
+
+
+        saveButton.setOnClickListener(e -> {
+            Log.d(TAG, "onClick: saveButton dialog");
             getDialog().dismiss();
         });
 
-        timeButton.setOnClickListener(e ->{
-            Log.d(TAG,"onClick: timeButton dialog");
+        timeButton.setOnClickListener(e -> {
+            Log.d(TAG, "onClick: timeButton dialog");
             TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
                 @Override
                 public void onTimeSet(TimePicker timePicker, int i, int i1) {
-                    hour= i;
+                    hour = i;
                     minute = i1;
                     time_selected = hour + ":" + minute;
 
                     SimpleDateFormat f24Hour = new SimpleDateFormat("HH:mm");
-                    try{
+                    try {
                         Date date = f24Hour.parse(time_selected);
                         timeButton.setText(f24Hour.format(date));
                     } catch (ParseException e) {
                         e.printStackTrace();
                     }
                 }
-            },24,0,true);
-            timePickerDialog.updateTime(hour,minute);
+            }, 24, 0, true);
+            timePickerDialog.updateTime(hour, minute);
             timePickerDialog.show();
         });
 
         return view;
     }
 
+    private void filter(String text) {
+        ArrayList<FoodItem> filterList = new ArrayList<>();
+
+        for (FoodItem item : foodItems) {
+            if (item.getFood_item().toLowerCase().contains(text.toLowerCase()) || item.isSelected()) {
+                filterList.add(item);
+            }
+        }
+        displayItem = filterList;
+        updateList(filterList);
+
+    }
+
+    private void updateList(ArrayList<FoodItem> lists){
+        adapter = new DialogFoodAdapter(getContext(), R.layout.dialog_foodview, lists);
+        itemList.setAdapter(adapter);
+    }
 }
