ad8445a3beeccc17f5dc01a95228cc1607af5cf4
Tawan <70558691+TawanLekngam@users.noreply.github.com>
2021-11-09 06:59:33 +0700

    Update get value from previous activity

--- app/src/main/java/com/gin_arai_dee/DietDailyPage.java
+++ app/src/main/java/com/gin_arai_dee/DietDailyPage.java
@@ -1,5 +1,6 @@
 package com.gin_arai_dee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -25,32 +26,36 @@ public class DietDailyPage extends AppCompatActivity implements DietDialog.OnInp
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_page_daily);
+        Intent intent = getIntent();
+        String selected_Day = intent.getStringExtra(DietPage.EXTRA_TEXT);
 
         foodItemLists = new ArrayList<>();
 
         selectedDay = findViewById(R.id.selectedDay);
+        selectedDay.setText(selected_Day);
+
         totalKcal = findViewById(R.id.total_kcal);
         recyclerView = findViewById(R.id.item_list);
 
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        cardAdapter = new CardDietAdapter(this,foodItemLists);
+        cardAdapter = new CardDietAdapter(this, foodItemLists);
         recyclerView.setAdapter(cardAdapter);
 
         add_item_button = findViewById(R.id.add_item_button);
         add_item_button.setOnClickListener(e -> {
             DietDialog dialog = new DietDialog();
-            dialog.show(getSupportFragmentManager(),"MyDialog");
+            dialog.show(getSupportFragmentManager(), "MyDialog");
         });
     }
 
     @Override
     public void sentInput(String time, ArrayList<FoodItem> lists) {
-        Log.d("DailyDeit","SendInput");
+        Log.d("DailyDeit", "SendInput");
         CardDietModel dietModel = new CardDietModel(time);
         //dietModel.setFoodItemsLists(lists);
         foodItemLists.add(dietModel);
-        cardAdapter = new CardDietAdapter(this,foodItemLists);
+        cardAdapter = new CardDietAdapter(this, foodItemLists);
         recyclerView.setAdapter(cardAdapter);
 
     }
