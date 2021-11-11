52ef5163361fb2f1bd99b10e9e4a8ae715dcdf4a
deeckn <dee28511@gmail.com>
2021-11-11 17:29:06 +0700

    Implemented swipe decorator on recyclerview cards

--- app/src/main/java/com/gin_arai_dee/UserPage.java
+++ app/src/main/java/com/gin_arai_dee/UserPage.java
@@ -1,11 +1,20 @@
 package com.gin_arai_dee;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.util.ArrayList;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
 public class UserPage extends AppCompatActivity {
 
@@ -56,6 +65,9 @@ public class UserPage extends AppCompatActivity {
         favoritesList = findViewById(R.id.favorites_recyclerview);
         favoritesAdapter = new CardAdapter(this, favorites);
         favoritesList.setAdapter(favoritesAdapter);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(swipeCallback);
+        itemTouchHelper.attachToRecyclerView(favoritesList);
     }
 
     // Opens the Home Page
@@ -72,4 +84,40 @@ public class UserPage extends AppCompatActivity {
     private void openFoodHub() {
         startActivity(new Intent(this, FoodHub.class));
     }
+
+    ItemTouchHelper.SimpleCallback swipeCallback = new ItemTouchHelper.SimpleCallback(0,
+            ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView,
+                              @NonNull RecyclerView.ViewHolder viewHolder,
+                              @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int id = favorites.get(viewHolder.getBindingAdapterPosition()).getId();
+            System.out.println(id);
+            if (direction == ItemTouchHelper.LEFT) {
+                db.updateFavoriteStatus(id, 0);
+                favorites.remove(viewHolder.getBindingAdapterPosition());
+                Toast.makeText(UserPage.this, "Removed from favorites", Toast.LENGTH_SHORT).show();
+            }
+            favoritesAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView,
+                                @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(UserPage.this, R.color.wine))
+                    .addSwipeLeftActionIcon(R.drawable.ic_baseline_delete_24)
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
